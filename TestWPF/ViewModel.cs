60753c773f0fef2a6dc3a7ac8ecc5a3d0cf1520a
Neo Xiao <my522cn@hotmail.com>
2020-12-12 10:41:57 +0800

    添加项目文件。

---
+++ TestWPF/ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.IO;
+
+namespace TestWPF
+{
+    class ViewModel : ViewModelBase
+    {
+
+        private string _Text;
+        public string Text
+        {
+            get => _Text;
+            set { Set(ref _Text, value); }
+        }
+
+        public ICommand RunCommand => new RelayCommand(() =>
+        {
+            Main();
+        });
+
+        public async void Main()
+        {
+            string code1 = File.ReadAllText("TestMain.cs");
+            await Task.Run(async () =>
+            {
+                var script = CSharpScript.RunAsync(code1 + "var tmain = new TestMain();", ScriptOptions.Default.AddReferences(typeof(TSBase).Assembly).AddImports("TestWPF")).Result;
+                await script.ContinueWithAsync("tmain.Run();");
+                var state1 = script.ContinueWithAsync<string>("tmain.Logger");
+                Text += state1.Result.ReturnValue + "\n===========\n";
+            });
+
+            await Task.Run(async () =>
+            {
+                var script = CSharpScript.RunAsync(code1 + "var tmain = new TestMain();", ScriptOptions.Default.AddReferences(typeof(TSBase).Assembly).AddImports("TSBase")).Result;
+                await script.ContinueWithAsync("tmain.Run();");
+                var state1 = script.ContinueWithAsync<string>("tmain.Logger");
+                Text += state1.Result.ReturnValue + "\n===========\n";
+            });
+
+        }
+
+    }
+}
