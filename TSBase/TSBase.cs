a641601f8565e078b0854dfe40fdf0e425172c49
Neo Xiao <my522cn@hotmail.com>
2020-12-12 10:41:57 +0800

    添加项目文件。

---
+++ TSBase/TSBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TSBase
+{
+    public class TSBase
+    {
+        public string Logger { get; set; }
+        public bool PanelResult { get; set; }
+
+        public (bool result, string log) Test(string testname)
+        {
+            Logger += $"{testname} test passed";
+            return (true, $"{testname} test passed");
+        }
+
+        public virtual void Run()
+        {
+            Logger = "Test Sequence is null";
+        }
+    }
+}
